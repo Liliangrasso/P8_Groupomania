8d7313b07734763820c93c6af40c4444a1d62bf7
Liliangrasso <lilian.grasso.pro@gmail.com>
2022-02-15 17:29:03 +0100

    :pencil: Création du serveur Http

---
+++ Backend/app.js
@@ -0,0 +1,44 @@
+﻿const express = require('express');
+
+const app = express();
+
+
+//Nous donne accée au Cors de la requète(Remplace Body-parser)
+app.use(express.json())
+
+// Access Header
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    res.status(201).json({message: 'Ajout du Post'})
+})
+
+//Récupération des Posts
+app.get('/api/stuff', (req, res, next) => {
+    const posts = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            likes: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            like: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(posts);
+});
+
+module.exports = app;
